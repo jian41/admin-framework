b2823d1624e071ac2b15fa1d8020ece833872a4d
mxvc <410518072@qq.com>
2025-11-19 21:32:48 +0800

    表单注册

--- web/config/plugins/admin-form.ts
+++ web/config/plugins/admin-form.ts
@@ -1,29 +1,39 @@
 import {IApi} from 'umi';
 import * as fs from "fs";
+import * as path from "path";
 
 // 自动注册src/forms下的表单
 export default (api: IApi) => {
-    console.log('form-register-by-dir running... ')
+    api.logger.info('plugin starting... ')
+    api.logger.info('info', JSON.stringify(api.env))
+
     api.describe({
         key: 'form-register-by-dir',
     });
+
     api.addEntryImports(() => ({
-        source: api.cwd + '/src/framework/system/formRegistry',
+        source: path.join( api.paths.absSrcPath , 'framework/system/formRegistry'),
         specifier: 'formRegistry'
     }))
 
-    const dir = api.cwd + "/src/forms"
+    const dir = path.join(api.paths.absSrcPath , 'forms')
+    api.logger.info('scan dir is',dir)
     fs.readdirSync(dir).forEach(file => {
-        if (file.endsWith(".jsx")) {
-            let source = api.cwd + '/src/forms/' + file;
-            console.log('form source path is ' + source)
-            let name = file.replace(".jsx", "");
-            api.addEntryImports(() => ({
-                source: source,
-                specifier: name
-            }))
-            api.addEntryCodeAhead(() => `formRegistry.register("${name}",${name} );`)
+        if (!file.endsWith(".jsx")) {
+            return;
         }
+        let source = path.join(dir,file) ;
+        let name = file.replace(".jsx", "");
+        console.log('form info: ' , name,source)
+
+        // import form
+        api.addEntryImports(() => ({
+            source: source,
+            specifier: name
+        }))
+
+        // register form
+        api.addEntryCodeAhead(() => `formRegistry.register("${name}",${name} );`)
     });
 
 };
