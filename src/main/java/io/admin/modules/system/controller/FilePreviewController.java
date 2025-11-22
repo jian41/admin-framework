d0b5a07e76f88746490d63a6fa510c40c9e9737e
mxvc <410518072@qq.com>
2025-11-22 23:18:21 +0800

    重构组件

--- src/main/java/io/admin/modules/system/controller/FilePreviewController.java
+++ src/main/java/io/admin/modules/system/controller/FilePreviewController.java
@@ -37,9 +37,12 @@ public class FilePreviewController {
     /**
      * 文件预览入口
      */
-    @GetMapping({"admin/sysFile/preview/{id}",
+    @GetMapping({
+            "preview/{id}",
+            "admin/preview/{id}",
+
             // 兼容老系统
-            "preview/{id}", "sysFile/preview/{id}","sysFile/preview/{id}.{suffix}"})
+            "sysFile/preview/{id}","sysFile/preview/{id}.{suffix}"})
     public ResponseEntity<StreamingResponseBody> previewFile(@PathVariable String id,
                                                              HttpServletRequest request,
                                                              Integer w, // 图片宽度
--- web/src/framework/field-components/FieldEditor.jsx
+++ web/src/framework/field-components/FieldEditor.jsx
@@ -22,7 +22,7 @@ export class FieldEditor extends React.Component {
 
                     // 上传图片
                     images_upload_url: uploadUrl,
-                    images_upload_base_path: '',
+                    images_upload_base_path: 'admin',
                     promotion: false, // 不显示升级按钮（右上角）
                     cache_suffix: '?v=v7.7',
 
